41345abf76f01076a81afc89ea26ee3c2566abd2
Agustiawan <44895587+agustiawan5209@users.noreply.github.com>
2025-09-07 00:55:14 +0800

    update : build

---
+++ public/build/assets/user-auth-layout-CdN26BWf.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DhGoMEv4.js";import{M as t}from"./main-layout-DA3azJqO.js";function i({children:s}){return r.jsx(t,{children:r.jsx("main",{className:"py-8",children:s})})}export{i as U};
--- public/build/assets/user-auth-layout-EBOF6Ar_.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-3gSJrcYZ.js";import{M as t}from"./main-layout-prN6Sfv_.js";function i({children:s}){return r.jsx(t,{children:r.jsx("main",{className:"py-8",children:s})})}export{i as U};
